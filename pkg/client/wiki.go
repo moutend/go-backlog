f78f1d3b6424ec9379bcd99308505258bb53d54d
Yoshiyuki Koyanagi <moutend@gmail.com>
2020-02-25 17:42:06 +0900

    Add methods

--- pkg/client/wiki.go
+++ pkg/client/wiki.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -14,6 +15,61 @@ import (
 const (
 	V2WikisPath = "/api/v2/wikis"
 )
+
+// AddWiki adds new Wiki page.
+//
+// For more details, see the API document.
+//
+// https://developer.nulab.com/docs/backlog/api/2/add-wiki-page/#add-wiki-page
+func (c *Client) AddWiki(wiki *Wiki, mailNotify bool) (*Wiki, error) {
+	return c.AddWikiContext(context.Background(), wiki, mailNotify)
+}
+
+// AddWikiContext accepts context.
+func (c *Client) AddWikiContext(ctx context.Context, wiki *Wiki, mailNotify bool) (*Wiki, error) {
+	path, err := c.root.Parse(V2WikisPath)
+
+	if err != nil {
+		return nil, err
+	}
+	if wiki == nil {
+		return nil, fmt.Errorf("wiki is required")
+	}
+
+	query := url.Values{}
+
+	query.Add("projectId", fmt.Sprint(wiki.ProjectId))
+	query.Add("name", fmt.Sprint(wiki.Name))
+	query.Add("content", fmt.Sprint(wiki.Content))
+
+	if mailNotify {
+		query.Add("mailNotify", "true")
+	}
+
+	payload := bytes.NewBufferString(query.Encode())
+
+	res, err := c.postContext(ctx, path, nil, payload)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var createdWiki *Wiki
+
+	if err := json.Unmarshal(body, &createdWiki); err != nil {
+		return nil, err
+	}
+
+	return createdWiki, nil
+}
 
 // GetWiki returns information about Wiki page.
 //
@@ -53,6 +109,100 @@ func (c *Client) GetWikiContext(ctx context.Context, wikiId uint64) (*Wiki, erro
 	}
 
 	return wiki, nil
+}
+
+// UpdateWiki updates information about Wiki page.
+//
+// For more details, see the API document.
+//
+// https://developer.nulab.com/docs/backlog/api/2/update-wiki-page/#update-wiki-page
+func (c *Client) UpdateWiki(wiki *Wiki, mailNotify bool) (*Wiki, error) {
+	return c.UpdateWikiContext(context.Background(), wiki, mailNotify)
+}
+
+// UpdateWikiContext accepts context.
+func (c *Client) UpdateWikiContext(ctx context.Context, wiki *Wiki, mailNotify bool) (*Wiki, error) {
+	path, err := c.root.Parse(path.Join(V2WikisPath, fmt.Sprint(wiki.Id)))
+
+	if err != nil {
+		return nil, err
+	}
+	if wiki == nil {
+		return nil, fmt.Errorf("wiki is required")
+	}
+
+	query := url.Values{}
+
+	query.Add("name", fmt.Sprint(wiki.Name))
+	query.Add("content", fmt.Sprint(wiki.Content))
+
+	if mailNotify {
+		query.Add("mailNotify", "true")
+	}
+
+	payload := bytes.NewBufferString(query.Encode())
+
+	res, err := c.patchContext(ctx, path, nil, payload)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var createdWiki *Wiki
+
+	if err := json.Unmarshal(body, &createdWiki); err != nil {
+		return nil, err
+	}
+
+	return createdWiki, nil
+}
+
+// DeleteWiki deletes wiki.
+//
+// For more details, see the API document.
+//
+// https://developer.nulab.com/docs/backlog/api/2/delete-wiki-page/#delete-wiki-page
+func (c *Client) DeleteWiki(wikiId uint64) (*Wiki, error) {
+	return c.DeleteWikiContext(context.Background(), wikiId)
+}
+
+// DeleteWikiContext accepts context.
+func (c *Client) DeleteWikiContext(ctx context.Context, wikiId uint64) (*Wiki, error) {
+	path, err := c.root.Parse(path.Join(V2WikisPath, fmt.Sprint(wikiId)))
+
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := c.deleteContext(ctx, path, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var deletedWiki *Wiki
+
+	if err := json.Unmarshal(body, &deletedWiki); err != nil {
+		return nil, err
+	}
+
+	return deletedWiki, nil
 }
 
 // GetWikis returns list of Wiki pages.
@@ -100,4 +250,98 @@ func (c *Client) GetWikisContext(ctx context.Context, projectIdOrKey string, que
 	}
 
 	return wikis, nil
+}
+
+// GetWikiCount returns number of Wiki pages.
+//
+// For more details, see the API document.
+//
+// https://developer.nulab.com/docs/backlog/api/2/count-wiki-page/#count-wiki-page
+func (c *Client) GetWikiCount(projectIdOrKey string) (int, error) {
+	return c.GetWikiCountContext(context.Background(), projectIdOrKey)
+}
+
+// GetWikiCountContext accepts context.
+func (c *Client) GetWikiCountContext(ctx context.Context, projectIdOrKey string) (int, error) {
+	path, err := c.root.Parse(path.Join(V2WikisPath, "count"))
+
+	if err != nil {
+		return -1, err
+	}
+
+	query := url.Values{}
+
+	if projectIdOrKey != "" {
+		query.Add("projectIdOrKey", projectIdOrKey)
+	}
+
+	res, err := c.getContext(ctx, path, query)
+
+	if err != nil {
+		return -1, err
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return -1, err
+	}
+
+	var result struct {
+		Count int `json:"count"`
+	}
+
+	if err := json.Unmarshal(body, &result); err != nil {
+		return -1, err
+	}
+
+	return result.Count, nil
+}
+
+// GetWikiTags returns list of tags that are used in the project.
+//
+// For more details, see the API document.
+//
+// https://developer.nulab.com/docs/backlog/api/2/get-wiki-page-tag-list/#get-wiki-page-tag-list
+func (c *Client) GetWikiTags(projectIdOrKey string) ([]*Tag, error) {
+	return c.GetWikiTagsContext(context.Background(), projectIdOrKey)
+}
+
+// GetWikiTagsContext accepts context.
+func (c *Client) GetWikiTagsContext(ctx context.Context, projectIdOrKey string) ([]*Tag, error) {
+	path, err := c.root.Parse(path.Join(V2WikisPath, "tags"))
+
+	if err != nil {
+		return nil, err
+	}
+
+	query := url.Values{}
+
+	if projectIdOrKey != "" {
+		query.Add("projectIdOrKey", projectIdOrKey)
+	}
+
+	res, err := c.getContext(ctx, path, query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var tags []*Tag
+
+	if err := json.Unmarshal(body, &tags); err != nil {
+		return nil, err
+	}
+
+	return tags, nil
 }
--- pkg/client/wiki_test.go
+++ pkg/client/wiki_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/moutend/go-backlog/internal/testutil"
+	. "github.com/moutend/go-backlog/pkg/types"
 )
 
 func TestGetWikis(t *testing.T) {
@@ -25,6 +26,30 @@ func TestGetWikis(t *testing.T) {
 	log.Printf("GetWikis: %+v\n", ws)
 }
 
+func TestAddWiki(t *testing.T) {
+	client, err := New(testutil.BacklogSpace, testutil.BacklogToken)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	client.SetHTTPClient(testutil.NewTestClient([]byte(`{}`), testutil.EnableHTTPRequest))
+
+	wiki := &Wiki{
+		Name:      "wiki name",
+		Content:   "wiki content",
+		ProjectId: 1234567,
+	}
+
+	createdWiki, err := client.AddWiki(wiki, false)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Printf("AddWiki: %+v\n", createdWiki)
+}
+
 func TestGetWiki(t *testing.T) {
 	client, err := New(testutil.BacklogSpace, testutil.BacklogToken)
 
@@ -41,4 +66,83 @@ func TestGetWiki(t *testing.T) {
 	}
 
 	log.Printf("GetWiki: %+v\n", w)
+}
+
+func TestUpdateWiki(t *testing.T) {
+	client, err := New(testutil.BacklogSpace, testutil.BacklogToken)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	client.SetHTTPClient(testutil.NewTestClient([]byte(`{}`), testutil.EnableHTTPRequest))
+
+	wiki := &Wiki{
+		Id:        7777777,
+		Name:      "Wiki name",
+		Content:   "Wiki content",
+		ProjectId: 1234567,
+	}
+
+	updatedWiki, err := client.UpdateWiki(wiki, false)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Printf("UpdateWiki: %+v\n", updatedWiki)
+}
+
+func TestDeleteWiki(t *testing.T) {
+	client, err := New(testutil.BacklogSpace, testutil.BacklogToken)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	client.SetHTTPClient(testutil.NewTestClient([]byte(`{}`), testutil.EnableHTTPRequest))
+
+	deletedWiki, err := client.DeleteWiki(7777777)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Printf("DeleteWiki: %+v\n", deletedWiki)
+}
+
+func TestGetWikiCount(t *testing.T) {
+	client, err := New(testutil.BacklogSpace, testutil.BacklogToken)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	client.SetHTTPClient(testutil.NewTestClient([]byte(`{}`), testutil.EnableHTTPRequest))
+
+	count, err := client.GetWikiCount("projectKey")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Printf("GetWikiCount: %+v\n", count)
+}
+
+func TestGetWikiTags(t *testing.T) {
+	client, err := New(testutil.BacklogSpace, testutil.BacklogToken)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	client.SetHTTPClient(testutil.NewTestClient([]byte(`[]`), testutil.EnableHTTPRequest))
+
+	tags, err := client.GetWikiTags("projectKey")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Printf("GetWikiTags: %+v\n", tags)
 }
